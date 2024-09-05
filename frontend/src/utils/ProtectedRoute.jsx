673602fc41e6b09107b64f3c910d1f5b13f0d19d
himanishpuri <himanishpuri2203@gmail.com>
2024-09-05 23:37:38 +0530

    hello world :

--- frontend/src/utils/ProtectedRoute.jsx
+++ frontend/src/utils/ProtectedRoute.jsx
@@ -11,7 +11,10 @@ function ProtectedRoute({ children }) {
 		try {
 			await axios.post(
 				"https://todo-list-app-xxh1.vercel.app/api/user/isValidUser",
-				{ withCredentials: true },
+				// {},
+				{
+					withCredentials: true,
+				},
 			);
 			setAuthenticated(true);
 		} catch (error) {
